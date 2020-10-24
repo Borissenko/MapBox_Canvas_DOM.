f15c83407470160acd724f870cf77379897881dd
borisenko <kk>
2020-10-24 11:37:36 +0300

    with <link>  all of this is work!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,8 @@
 <script>
 import mapboxgl from 'mapbox-gl'
 import MapboxDraw from "@mapbox/mapbox-gl-draw"
+import mapboxPolyline from '@mapbox/polyline'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'  //it is no work !
 
 export default {
   mounted() {
@@ -17,29 +19,45 @@ export default {
       style: 'mapbox://styles/mapbox/streets-v8',
       center: [37.618423, 55.751244],
       zoom: 12
-    });
+    })
   
-    let Draw = new MapboxDraw();
+    // let Draw = new MapboxDraw();
+    //
+    // // map.addControl(Draw, 'top-left')
+    // map.addControl(new mapboxgl.NavigationControl(),'top-right')
   
-    map.addControl(Draw, 'top-left');
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, "top-right")
+    
+    const marker = new mapboxgl.Marker()
+      .setLngLat([37.618423, 55.751244])
+      .addTo(map)
   
+    const geolocate = new mapboxgl.GeolocateControl({  //This locates the user on the map based on the browser's geolocation API.
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    })
+    map.addControl(geolocate, "top-right")
+    
     map.on('load', function() {
-      // ALL YOUR APPLICATION CODE
+      console.log('= load =')
     });
   }
 }
 </script>
 
 <style>
-
 body {
   margin: 0;
   padding: 0;
 }
 #map {
   position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 100%;
+  top: 10px;
+  left: 10px;
+  height: 90vh;
+  width: 95%;
 }
 </style>
